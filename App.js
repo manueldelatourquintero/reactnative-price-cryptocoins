399fa94b084086fb3712ebce05fc068c696a2af6
Manuel Gerardo Delatour Quintero <manuel.gerardo.delatour@gmail.com>
2021-06-22 15:19:45 -0500

    First Commit
    Versión en Producción de la aplicación

---
+++ App.js
@@ -0,0 +1,96 @@
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  FlatList,
+  TextInput,
+} from "react-native";
+
+import CoinItem from "./components/CoinItem";
+
+//Colocando titulo a la aplicacion
+//xd
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const loadData = async () => {
+    const res = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    );
+    const data = await res.json();
+    setCoins(data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#141414" />
+
+      <View style={styles.header}>
+        <Text style={styles.title}>CryptoMarket</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a Coin"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => text && setSearch(text)}
+        />
+      </View>
+
+      <FlatList
+        style={styles.list}
+        data={coins.filter(
+          (coin) =>
+            coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+            coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )}
+        showsVerticalScrollIndicator={false}
+        renderItem={({ item }) => <CoinItem coin={item} />}
+        refreshing={refreshing}
+        onRefresh={async () => {
+          setRefreshing(true);
+          await loadData();
+          setRefreshing(false);
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#141414",
+    flex: 1,
+    alignItems: "center",
+  },
+  header: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 20,
+    color: "#fff",
+    marginTop: 10,
+  },
+  list: {
+    width: "90%",
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
+  },
+});
+
+export default App;
